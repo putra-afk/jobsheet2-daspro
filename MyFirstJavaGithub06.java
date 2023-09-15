362aa0a49cb35f8b395532dedc76dfb68f89cf11
putra-afk <putralaksmanabayu@gmail.com>
2023-09-15 14:55:19 +0700

    Hi semangat Bayu!!

--- MyFirstJavaGithub06.java
+++ MyFirstJavaGithub06.java
@@ -1,6 +1,8 @@
 public class MyFirstJavaGithub06 {
 
-    public static void public static void main(String[] args) {
-        system.out.println(x:"hello from github");
+    public static void main(String[] args) {
+        System.out.println("hello from github");
+        System.out.println();
+        
     }
 }
